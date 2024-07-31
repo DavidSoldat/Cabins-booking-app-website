ce4129d3caa1bf3f32e0e2279ed909372bc49afe
David Soldat <davidsoldat00@gmail.com>
2024-07-31 21:35:41 +0200

    d

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,11 +1,17 @@
 import { Suspense } from 'react';
 import CabinList from '../_components/CabinList';
 import Spinner from '../_components/Spinner';
+import Filter from '../_components/Filter';
+
+export const revalidate = 3600;
 
 export const metadata = {
   title: 'Cabins',
 };
-export default function Page() {
+
+export default function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? 'all';
+
   return (
     <div>
       <h1 className='text-4xl mb-5 text-accent-400 font-medium'>
@@ -20,8 +26,12 @@ export default function Page() {
         Welcome to paradise.
       </p>
 
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className='flex justify-end mb-8'>
+        <Filter />
+      </div>
+
+      <Suspense key={filter} fallback={<Spinner />}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
